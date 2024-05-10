c3d8078a815f287d7ffe46ee2b6fa38d3c642f7f
Adair Bernal <adairoswaldo01@gmail.com>
2024-05-09 19:33:53 -0700

    description fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import Footer from "@/components/main/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
-  description: "Web Portfolio made on Next.js",
+  title: "Adair Bernal's Portfolio",
+  description: "made on Next.js",
 };
 
 export default function RootLayout({
